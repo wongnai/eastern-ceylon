d1b7887f106eda82d6b49b8020f9d0278365228a
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2020-02-13 15:27:40 +0700

    Initial commit

---
+++ eastern_ceylon/plugin.py
@@ -0,0 +1,33 @@
+import warnings
+import json
+
+from eastern import EasternPlugin
+
+ENV_KEY = '__ceylon'
+
+
+class CeylonPlugin(EasternPlugin):
+    def env_hook(self, formatter, **kwargs):
+        if ENV_KEY in formatter.env:
+            # For recursive format, don't run this plugin again
+            return {}
+
+        vars_file = formatter.path / 'vars.json'
+        namespace = formatter.env['NAMESPACE']
+
+        if not vars_file.is_file():
+            warnings.warn('Ceylon: vars.json file not found!')
+            return {}
+
+        with vars_file.open() as fp:
+            variables = json.load(fp)
+
+            out = {}
+            if namespace in variables:
+                out = variables[namespace]
+            elif 'default' in variables:
+                out = variables['default']
+            
+            out[ENV_KEY] = True
+            
+            return out
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eastern-ceylon',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'eastern>=2.0',
+    ],
+    entry_points={
+        'eastern.plugin': ['ceylon = eastern_ceylon.plugin:CeylonPlugin']
+    })
