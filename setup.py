c6bb4ebb594996b3c210dc11b5b035919ffa6210
Manatsawin Hanmongkolchai <git@whs.in.th>
2021-01-11 10:10:48 +0700

    Reformat setup

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='eastern-ceylon',
-    version='1.0',
+    name="eastern-ceylon",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        'eastern>=2.0',
+        "eastern>=2.0",
     ],
-    entry_points={
-        'eastern.plugin': ['ceylon = eastern_ceylon.plugin:CeylonPlugin']
-    })
+    entry_points={"eastern.plugin": ["ceylon = eastern_ceylon.plugin:CeylonPlugin"]},
+)
